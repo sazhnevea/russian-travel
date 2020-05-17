353fd77cb5f845f0aa2dc5137c17d6d14a2a2106
Evgeniy Sazhnev <evgeniy.sazhnev@simbirsoft.com>
2020-05-17 17:07:39 +0400

    added plugins to go online

--- server/server.js
+++ server/server.js
@@ -3,7 +3,7 @@ const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-const publicPath = path.join(__dirname, '..', 'build');
+const publicPath = path.join(__dirname, '..');
 
 app.use(express.static(publicPath));
 
