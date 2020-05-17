7643a30e1f1f2c8e32a9befc1ff356e670de1ec0
Evgeniy Sazhnev <evgeniy.sazhnev@simbirsoft.com>
2020-05-17 17:00:52 +0400

    added server to listen a port

---
+++ server/server.js
@@ -0,0 +1,16 @@
+const path = require('path');
+const express = require('express');
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+const publicPath = path.join(__dirname, '..', 'build');
+
+app.use(express.static(publicPath));
+
+app.get('*', (req, res) => {
+    res.sendFile(path.join(publicPath, 'index.html'))
+})
+
+app.listen(PORT, () => {
+    console.log(`Server has been started on port ${PORT}`);
+})
